acc04aaa3e91f7a43cd823cbe3c8625508fe63eb
Daniel Wulff <danielwulff.ee@gmail.com>
2016-09-12 21:59:42 -0700

    finished game

--- Roll_a_ball-Wulff/Assets/Scripts/PlayerController.cs
+++ Roll_a_ball-Wulff/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
     public float speed = 10;
+    private int count;
+    public Text countText;
+    public Text winText;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-	}
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -30,6 +37,17 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("Shiny"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 14)
+        {
+            winText.text = "YAYYYY!!!!";
         }
     }
 }
