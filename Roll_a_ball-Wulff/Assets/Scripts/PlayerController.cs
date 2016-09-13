2e83b9ea314462dec02cfd61374fc61315635689
Daniel Wulff <danielwulff.ee@gmail.com>
2016-09-12 21:09:16 -0700

    well... it rolls

---
+++ Roll_a_ball-Wulff/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Shiny"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
